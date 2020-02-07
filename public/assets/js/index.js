31e7bb9b78eace0e2e319e331526ad7b5250beb9
Dicky Julian <julian18.ech@gmail.com>
2020-02-07 10:16:13 +0700

    FE first commit

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,8 +4,22 @@ window.onscroll = function () {
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#index-navbar").css('boxShadow','0 4px 6px -1px rgba(0,0,0,0.07)')
+        $("#index-navbar").css('boxShadow', '0 4px 6px -1px rgba(0,0,0,0.07)')
     } else {
-        $("#index-navbar").css('boxShadow','none')
+        $("#index-navbar").css('boxShadow', 'none')
     }
+}
+
+function typeActivate(obj) {
+    $('.product-container').remove();
+    $('.h3-xhsdbz').remove();
+    $('.hr-xhsdbz').remove();
+    $('.container-flex').find('h3:first').html('Lelang '+ $(obj).html());
+    $('.type-active').removeClass('type-active');
+    $(obj).addClass('type-active');
+}
+
+
+function getURLParameter(url, name) {
+    return (RegExp(name + '=' + '(.+?)(&|$)').exec(url) || [, null])[1];
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,30 +4,30 @@
 <div id="app">
     <div id="type-thing" class="text-center mt-3">
         <div class="d-i-block">
-            <a href="" class="p-2 type-active">All</a>
+            <a href="/" class="p-2 type-active">All</a>
         </div>
         <div class="d-i-block">
-            <a href="" class="p-2">Bangunan</a>
+            <a href="#?type=bangunan" class="p-2" onclick="typeActivate(this)">Bangunan</a>
         </div>
         <div class="d-i-block">
-            <a href="" class="p-2">Mesin</a>
+            <a href="#?type=mesin" class="p-2" onclick="typeActivate(this)">Mesin</a>
         </div>
         <div class="d-i-block">
-            <a href="" class="p-2">Perhiasan</a>
+            <a href="#?type=perhiasan" class="p-2" onclick="typeActivate(this)">Perhiasan</a>
         </div>
         <div class="d-i-block">
-            <a href="" class="p-2">Koleksi</a>
+            <a href="#?type=koleksi" class="p-2" onclick="typeActivate(this)">Koleksi</a>
         </div>
         <div class="d-i-block">
-            <a href="" class="p-2">Kawat</a>
+            <a href="#?type=kawat" class="p-2" onclick="typeActivate(this)">Kawat</a>
         </div>
         <div class="d-i-block">
-            <a href="" class="p-2">Balung</a>
+            <a href="#?type=balung" class="p-2" onclick="typeActivate(this)">Balung</a>
         </div>
     </div>
 
-    <hr class="mt-3 mb-4">
-    <h3 class="title-content">Paling Banyak Dicari</h3>
+    <hr class="mt-3 mb-4 hr-xhsdbz">
+    <h3 class="title-content h3-xhsdbz">Paling Banyak Dicari</h3>
     <div class="product-container d-flex">
         <a class="product p-1 text-center c-pointer">
             <div class="product-img background-setup" style="background: url(assets/img/product/jbl.png)"></div>
@@ -51,10 +51,11 @@
         </a>
     </div>
 
-    <h3 class="title-content mt-5">Mungkin Anda Butuhkan</h3>
-    <div class="d-flex">
+    
+    <div class="d-flex container-flex">
         <div class="product-list">
-
+        <h3 class="title-content mt-5">Mungkin Anda Butuhkan</h3>
+        <hr>
         <div class="product d-flex mb-4">
                 <a class="product-info" href="">
                     <h3 class="mt-1 mb-1">JBL BT565BTNC - Headphone over-ear dengan noise cacelling</h3>
@@ -105,8 +106,63 @@
                     <div class="product-img background-setup float-right" style="background: url(assets/img/product/jbl.png)"></div>
                 </div>
             </div>
-            <button class="bt-medium fullwidth bt-primary c-pointer">Lihat Lebih Banyak</button>
+            <div class="product d-flex mb-4">
+                <a class="product-info" href="">
+                    <h3 class="mt-1 mb-1">JBL BT565BTNC - Headphone over-ear dengan noise cacelling</h3>
+                    <h4 class="mt-1 mb-1 text-thin">Start Bidding IDR 200.000</h4>
+                    <h4 class="color-pale text-thin mt-1">Jakarta Raya</h4>
+                </a>
+                <div class="product-img-container">
+                    <div class="product-img background-setup float-right" style="background: url(assets/img/product/jbl.png)"></div>
+                </div>
+            </div>
+            <button class="bt-medium fullwidth bt-primary c-pointer mb-4">Lihat Lebih Banyak</button>
         </div>
+
+        <div id="add-new">
+            <div class="add-new content">
+                <div class="find-by-kategori pl-4">
+                    <h3 class="title-content mt-5 text-center">Telusuri dengan Kategori</h3>
+                    <hr>
+                    <div class="tag-list mt-4">
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>techno
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>technology
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>techno
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>technology
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>techno
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>technology
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>techno
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>techno
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>techno
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>techno
+                        </a> 
+                        <a href="" class="tag m-05">
+                            <div class="color-primary">#</div>techno
+                        </a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
     
 
